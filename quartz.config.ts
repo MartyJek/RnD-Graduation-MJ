6b837b9d977397e504ef998d3fdfec45731bc99b
Onwaycation <88710673+MartyJek@users.noreply.github.com>
2023-09-13 19:26:03 +0200

    Quartz sync: Sep 13, 2023, 7:26 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "https://martyjek.github.io/DC-Research-MJ/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
